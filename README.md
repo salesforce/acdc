9829998c83fb452056cdd71452069e0f985289d0
Simon Chow <simon.chow@salesforce.com>
2022-01-31 15:20:29 -0800

    authorization config hashed-keys from env variable

--- README.md
+++ README.md
@@ -16,3 +16,25 @@ documentation](https://www.playframework.com/documentation/2.8.x/Production) on
 how to deploy the service.
 
 To setup the database run `com.salesforce.mce.acdc.tool.ProvisionDatabase`.
+
+```shell 
+sbt 'project core; run ProvisionDatabase'
+```
+
+If config ```acdc.auth.enabled = true``` is enabled, authorization api-keys are SHA-256 hashed.  The REST enb-points should include a http header key ```x-api-key``` with appropriate value.  
+
+Set an environment variable for the authorized key value pairs.  Example:
+
+```shell
+export MCE_ENV_X_API_KEYS="{f6e42a3c0dffee079face0da061ee2c9a871eebe098ac481248e34cfe023955b = [admin, user], 70ad8c1543728a3e61bbec26d1df5bd742d1c2f464f2ac54a2fec5e709eba890 = [admin]}"
+```
+
+In the above example, ```f6e42a3c0dffee079face0da061ee2c9a871eebe098ac481248e34cfe023955b``` is a
+hashed api-key, and ```admin``` and ```user``` are access roles.  More than one api-key can be included in the environment variable ```MCE_ENV_X_API_KEYS``` for keys rotation or 
+keys with different access roles.
+
+## Run web service 
+
+```shell 
+sbt ws/run
+```
--- acdc-ws/app/utils/AuthorizationSettings.scala
+++ acdc-ws/app/utils/AuthorizationSettings.scala
@@ -2,7 +2,7 @@ package utils
 
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
-import com.typesafe.config.{Config, ConfigFactory, ConfigList}
+import com.typesafe.config.{Config, ConfigFactory}
 
 class AuthorizationSettings private (config: Config) {
 
@@ -13,14 +13,50 @@ class AuthorizationSettings private (config: Config) {
   def keyRoles: Map[String, List[String]] = config
     .entrySet()
     .asScala
-    .filter(_.getKey.startsWith(s"hashed-keys."))
-    .map(kv => {
-      val key: String = kv.getKey.stripPrefix(s"hashed-keys.")
-      val configList: ConfigList = kv.getValue.asInstanceOf[ConfigList]
-      val listValue = configList.unwrapped().asScala.map(_.toString).toList
-      (key, listValue)
+    .filter(_.getKey.startsWith(s"hashed-keys"))
+    .flatMap(kv => {
+      val stripQuote = kv.getValue.toString.stripPrefix("Quoted(\"").stripSuffix("\")")
+      getHashedKeysMap(stripQuote)
     })
     .toMap
+
+  /**
+   * regex parse input string to a map of x-api-key, value being a list of associated roles
+   *
+   * @param s  input string expected of this format
+   *           "{ f6e42a3c0dffee079face0da061ee2c9a871eebe098ac481248e34cfe023955b = [admin, user]
+   *           , 70ad8c1543728a3e61bbec26d1df5bd742d1c2f464f2ac54a2fec5e709eba890 = [admin] }"
+   * @return map of x-api-key, value being a list of associated roles
+   */
+  def getHashedKeysMap(s: String): Map[String, List[String]] = {
+    // regex: { hashed_key1 = [role1, role2], key2 = [role3, role4] }
+    lazy val keyVal = raw"[,\s{]*(\w+)\s*=\s*\[([\w\W]*?)]+?[\s}]*".r
+
+    // regex for role1, role2
+    lazy val valList = raw"([\s,]*)([^\s,]+)\s*".r
+
+    val s2 = s.replaceAll("""\\""" + """\"""", "").replaceAll("""\"""", "")
+    println(s"AuthorizationSettings.getHashedKeysMap: s=$s")
+    println(s"AuthorizationSettings.getHashedKeysMap: s2=$s2")
+    keyVal
+      .findAllIn(s2)
+      .map {
+        // key would be a hashed key of a x-api-key; vList would be roles (example: admin, user)
+        case keyVal(key, vList) =>
+          (
+            key,
+            valList
+              .findAllIn(vList)
+              .map {
+                case valList(_, v) => v // ignore the comma separator
+                case _ => None.asInstanceOf[String]
+              }
+              .toList
+          )
+        case _ => ().asInstanceOf[Tuple2[String, List[String]]]
+      }
+      .toMap
+  }
 
 }
 
